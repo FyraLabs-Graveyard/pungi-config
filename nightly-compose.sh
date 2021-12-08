d16592c6601cfb492fd35d1447dc9b3a95d9b5c3
Cappy Ishihara <cappy@cappuchino.xyz>
2021-12-08 10:41:09 +0700

    added nightly composes

---
+++ nightly-compose.sh
@@ -0,0 +1,4 @@
+find /mnt/koji/compose/35/* -mtime +7 -exec rm {} \;
+pungi-koji --config ultramarine.conf --target-dir /mnt/koji/compose/35/ --nightly\
+ --skip-phase live_media\
+ --old-composes /mnt/koji/compose/35
