3b26c6532bac037b7d8bbd1409e8a3353f7bdc5c
root <root@lapis.ultramarine-linux.org>
2022-01-30 21:56:53 -0500

    funny

--- test-compose.sh
+++ test-compose.sh
@@ -1,1 +1,3 @@
-pungi-koji --config ultramarine-testing.conf --target-dir /mnt/koji/compose/testing/ --old-composes=/mnt/koji/compose/testing $@
+DATE=$(date "+%Y%m%d")
+pungi-koji --config ultramarine-testing.conf --target-dir /mnt/koji/compose/testing/ --old-composes=/mnt/koji/compose/testing --skip-phase=productimg $@
+
